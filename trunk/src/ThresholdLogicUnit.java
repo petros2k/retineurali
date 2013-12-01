f8550cf237bc1a1a2708f2cd11d09b55fffd44eb
danielecaso <danielecaso@85a17db5-8751-0b57-a92e-1e3f5cdb97f0>
2013-12-01 20:31:59 +0000

    Stesura classe ThresholdLogicUnit

---
+++ trunk/src/ThresholdLogicUnit.java
@@ -0,0 +1,21 @@
+
+public class ThresholdLogicUnit {
+
+	private double weightedSum;
+	private double activation;
+	private double bias;
+
+	public ThresholdLogicUnit(){
+		weightedSum = 0.0;
+		activation = 0.0;
+
+	}
+
+
+	public void calculateWeightedSum(double[] inputs, double[] weights, double bias){
+
+		weightedSum=0.0;
+
+
+	}
+}
