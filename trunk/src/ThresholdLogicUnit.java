d1b127d798b25bb4011ca93d6fe09c603e936950
danielecaso <danielecaso@85a17db5-8751-0b57-a92e-1e3f5cdb97f0>
2013-12-02 20:48:58 +0000

    Implementazione del TLU (Threshold Logico Unit)

--- trunk/src/ThresholdLogicUnit.java
+++ trunk/src/ThresholdLogicUnit.java
@@ -1,8 +1,13 @@
+//SINGOLA UNITA' DI ELABORAZIONE DELLA RETE NEURALE
+
 
 public class ThresholdLogicUnit {
 
 	private double weightedSum;
+
+
 	private double activation;
+
 	private double bias;
 
 	public ThresholdLogicUnit(){
@@ -12,10 +17,64 @@ public class ThresholdLogicUnit {
 	}
 
 
+	//CALCOLO DELLA SOMMA PESATA
 	public void calculateWeightedSum(double[] inputs, double[] weights, double bias){
 
 		weightedSum=0.0;
 
+		if(inputs.length != weights.length){
+			System.out.println("Error: input size is different from weight size!");
+			return;
+		}
 
+		for(int i=0; i<inputs.length; i++){
+
+			weightedSum += inputs[i]*weights[i]; 
+		}
+
+		//AGGIUNGE IL BIAS
+		weightedSum += bias;
 	}
+
+	//CALCOLO DELLA FUNZIONE DI ATTIVAZIONE:SIGMOIDE
+	public void calculateSigmoidActivation(){
+		activation = 1 / Math.exp(-weightedSum);
+	}
+
+	//CALCOLO DELLA FUNZIONE DI ATTIVAZIONE:GAUSSIANA
+	public void calculateGaussianoActivation(){
+		activation = Math.exp(-(Math.pow(weightedSum,2)));
+	}
+
+	//CALCOLO DELLA FUNZIONE DI ATTIVAZIONE:SGN
+	public void calculateSgnActivation(){
+
+		activation = weightedSum>1 ? 1.0 : 0.0;
+	}
+
+	//FIRE DEL NEURONE 
+	public double fireNeurons(String type, double[] inputs, double[] weights, double bias){
+		
+		calculateWeightedSum(inputs, weights, bias);
+		
+		if(type.equalsIgnoreCase("sigmoid"))
+			calculateSigmoidActivation();
+		else if(type.equalsIgnoreCase("gaussian"))
+			calculateGaussianoActivation();
+		else
+			calculateSgnActivation();
+		
+		return activation;
+	}
+	
+	//METODI SET-GET
+	public double getActivation() {
+		return activation;
+	}
+
+	public double getWeightedSum() {
+		return weightedSum;
+	}
+
+
 }
